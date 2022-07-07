a7b671421664246510d7a50afbb7ec066d7c923d
liuq <lqadm@qq.com>
2022-07-07 19:20:25 +0800

    chore: add centos 7.x update kernel

---
+++ sh/centos-7.x-update-kernel.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+#=================================================================#
+#   System Required:  CentOS 7  v0.0.1                            #
+#   Description: Update kernel                                    #
+#   Description: CentOS系统内核                                    #
+#=================================================================#
+
+clear
+
+# Colors
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+kernel=$(uname -sr)
+
+echo ""
+echo -e "Current kernel version: ${yellow}$kernel${plain}"
+echo ""
+read -n2 -p "Are you sure to update kernel [Y/N]?" answer
+
+case $answer in
+(Y | y)
+  echo ""
+  echo -e "${green}- Update kernel${plain}"
+  echo ""
+  rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+  rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+  yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
+
+  echo ""
+  echo -e "${green}- Install kernel${plain}"
+  echo ""
+  yum --enablerepo=elrepo-kernel install kernel-lt -y
+
+  echo ""
+  echo -e "${green}- Set default${plain}"
+  echo ""
+  sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/g' /etc/default/grub
+
+  echo ""
+  echo -e "${green}- Regenerate the grub configuration file${plain}"
+  echo ""
+  grub2-mkconfig -o /boot/grub2/grub.cfg
+
+  echo ""
+  echo -e "${green}- Reboot wait 5s${plain}"
+  echo ""
+  sleep 5s
+
+  reboot
+;;
+esac
