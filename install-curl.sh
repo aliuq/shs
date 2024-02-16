7cf61dfc728d61474f9f43ed32be814b69661c1e
liuqian <liuqian@gdcattsoft.com>
2024-02-17 06:12:35 +0800

    test: test input params

--- install-curl.sh
+++ install-curl.sh
@@ -8,26 +8,28 @@ set -e
 
 CURL_VERSION=${1:-8.6.0}
 
-apk add --update --no-cache openssl-dev nghttp2-dev ca-certificates libpsl-dev
-apk add --update --no-cache --virtual curldeps g++ make perl
+echo "Installing curl version $CURL_VERSION"
 
-wget https://curl.haxx.se/download/curl-$CURL_VERSION.tar.bz2 -O /tmp/curl-$CURL_VERSION.tar.bz2 && \
-cd /tmp && \
-tar xjvf curl-$CURL_VERSION.tar.bz2 && \
-rm curl-$CURL_VERSION.tar.bz2 && \
-cd curl-$CURL_VERSION && \
-./configure \
-  --with-nghttp2=/usr \
-  --prefix=/usr \
-  --with-ssl \
-  --enable-ipv6 \
-  --enable-unix-sockets \
-  --without-libidn \
-  --disable-static \
-  --disable-ldap \
-  --with-pic && \
-make && \
-make install && \
-cd / && \
-rm -rf /tmp/curl-$CURL_VERSION && \
-apk del curldeps
+# apk add --update --no-cache openssl-dev nghttp2-dev ca-certificates libpsl-dev
+# apk add --update --no-cache --virtual curldeps g++ make perl
+
+# wget https://curl.haxx.se/download/curl-$CURL_VERSION.tar.bz2 -O /tmp/curl-$CURL_VERSION.tar.bz2 && \
+# cd /tmp && \
+# tar xjvf curl-$CURL_VERSION.tar.bz2 && \
+# rm curl-$CURL_VERSION.tar.bz2 && \
+# cd curl-$CURL_VERSION && \
+# ./configure \
+#   --with-nghttp2=/usr \
+#   --prefix=/usr \
+#   --with-ssl \
+#   --enable-ipv6 \
+#   --enable-unix-sockets \
+#   --without-libidn \
+#   --disable-static \
+#   --disable-ldap \
+#   --with-pic && \
+# make && \
+# make install && \
+# cd / && \
+# rm -rf /tmp/curl-$CURL_VERSION && \
+# apk del curldeps
