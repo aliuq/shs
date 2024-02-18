09007aa9e601b939910b9b81ff62c1ae700a3a57
liuqian <liuqian@gdcattsoft.com>
2024-02-18 19:09:58 +0800

    zsh: add install script on ubuntu

---
+++ zsh/ubuntu.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+
+# Usage
+#
+#
+#
+ZSH_VERSION=${1:-5.9}
+ZSH_ORIGIN=${ZSH_ORIGIN:-"https://zenlayer.dl.sourceforge.net"}
+
+echo """
+  Installing zsh version $ZSH_VERSION on $ZSH_ORIGIN
+
+  OS: Ubuntu
+  Requireed: curl make gcc libncurses5-dev libncursesw5-dev
+"""
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+check_commands() {
+  for cmd in "$@"; do
+    if ! command_exists "$cmd"; then
+      echo "Error: $cmd is not installed or not in PATH" >&2
+      exit 1
+    fi
+  done
+}
+
+install_deps() {
+  apt update
+  apt install -y curl make gcc libncurses5-dev libncursesw5-dev
+}
+
+install_zsh() {
+  curl -fsS -o /tmp/zsh.tar.xz $ZSH_ORIGIN/project/zsh/zsh/$ZSH_VERSION/zsh-$ZSH_VERSION.tar.xz
+  tar -xf /tmp/zsh.tar.xz -C /tmp
+  cd /tmp/zsh-$ZSH_VERSION
+  ./Util/preconfig && ./configure
+  make -j 20 install.bin install.modules install.fns
+  cd /
+  rm -f /tmp/zsh.tar.xz
+  rm -rf /tmp/zsh-$ZSH_VERSION
+}
+
+check_commands curl tar make gcc
+install_deps
+install_zsh
