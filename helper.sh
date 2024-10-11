c68f99b986fd454acfac769d8834f18778da407d
aliuq <aliuq@bilishare.com>
2024-10-11 09:55:22 +0800

    chore: Update shell helper script with new color options and functions

--- helper.sh
+++ helper.sh
@@ -28,12 +28,13 @@
 # + https://bit.ly/shell-helper-mirror
 
 # Colors
-bold="\033[1m"
-red="\033[0;31m"
-green="\033[0;32m"
-yellow="\033[0;33m"
-cyan="\033[0;36m"
-plain="\033[0m"
+# bold="\033[1m"
+# red="\033[0;31m"
+# green="\033[0;32m"
+# yellow="\033[0;33m"
+# cyan="\033[0;36m"
+# plain="\033[0m"
+# dim="\033[2m"
 
 verbose=false
 force=false
@@ -77,23 +78,80 @@ print_arg_warn() {
   fi
 }
 
+# =============== Colors ===============
+init() { printf "$1$3$2\n"; }
+
+reset() { init "\033[0m" "\033[0m" "$1"; }
+bold() { init "\033[1m" "\033[22m" "$1"; }
+dim() { init "\033[2m" "\033[22m" "$1"; }
+italic() { init "\033[3m" "\033[23m" "$1"; }
+underline() { init "\033[4m" "\033[24m" "$1"; }
+inverse() { init "\033[7m" "\033[27m" "$1"; }
+hidden() { init "\033[8m" "\033[28m" "$1"; }
+strikethrough() { init "\033[9m" "\033[29m" "$1"; }
+
+black() { init "\033[30m" "\033[39m" "$1"; }
+red() { init "\033[31m" "\033[39m" "$1"; }
+green() { init "\033[32m" "\033[39m" "$1"; }
+yellow() { init "\033[33m" "\033[39m" "$1"; }
+blue() { init "\033[34m" "\033[39m" "$1"; }
+magenta() { init "\033[35m" "\033[39m" "$1"; }
+cyan() { init "\033[36m" "\033[39m" "$1"; }
+white() { init "\033[37m" "\033[39m" "$1"; }
+gray() { init "\033[90m" "\033[39m" "$1"; }
+
+bg_black() { init "\033[40m" "\033[49m" "$1"; }
+bg_red() { init "\033[41m" "\033[49m" "$1"; }
+bg_green() { init "\033[42m" "\033[49m" "$1"; }
+bg_yellow() { init "\033[43m" "\033[49m" "$1"; }
+bg_blue() { init "\033[44m" "\033[49m" "$1"; }
+bg_magenta() { init "\033[45m" "\033[49m" "$1"; }
+bg_cyan() { init "\033[46m" "\033[49m" "$1"; }
+bg_white() { init "\033[47m" "\033[49m" "$1"; }
+
+black_bright() { init "\033[90m" "\033[39m" "$1"; }
+red_bright() { init "\033[91m" "\033[39m" "$1"; }
+green_bright() { init "\033[92m" "\033[39m" "$1"; }
+yellow_bright() { init "\033[93m" "\033[39m" "$1"; }
+blue_bright() { init "\033[94m" "\033[39m" "$1"; }
+magenta_bright() { init "\033[95m" "\033[39m" "$1"; }
+cyan_bright() { init "\033[96m" "\033[39m" "$1"; }
+white_bright() { init "\033[97m" "\033[39m" "$1"; }
+
+bg_black_bright() { init "\033[100m" "\033[49m" "$1"; }
+bg_red_bright() { init "\033[101m" "\033[49m" "$1"; }
+bg_green_bright() { init "\033[102m" "\033[49m" "$1"; }
+bg_yellow_bright() { init "\033[103m" "\033[49m" "$1"; }
+bg_blue_bright() { init "\033[104m" "\033[49m" "$1"; }
+bg_magenta_bright() { init "\033[105m" "\033[49m" "$1"; }
+bg_cyan_bright() { init "\033[106m" "\033[49m" "$1"; }
+bg_white_bright() { init "\033[107m" "\033[49m" "$1"; }
+
+print_colors() {
+  echo "颜色预览"
+  echo
+  echo "$(reset reset)  $(bold bold)  $(dim dim)  $(italic italic)  $(underline underline)  $(inverse inverse)  $(hidden hidden)  $(strikethrough strikethrough)"
+  echo "$(black black)  $(red red)  $(green green)  $(yellow yellow)  $(blue blue)  $(magenta magenta)  $(cyan cyan)  $(white white)  $(gray gray)"
+  echo "$(bg_black white bg_black)  $(bg_red bg_red)  $(bg_green bg_green)  $(bg_yellow bg_yellow)  $(bg_blue bg_blue)  $(bg_magenta bg_magenta)  $(bg_cyan bg_cyan)  $(bg_white bg_white)"
+  echo "$(black_bright black_bright)  $(red_bright red_bright)  $(green_bright green_bright)  $(yellow_bright yellow_bright)  $(blue_bright blue_bright)  $(magenta_bright magenta_bright)  $(cyan_bright cyan_bright)  $(white_bright white_bright)"
+  echo "$(bg_black_bright bg_black_bright)  $(bg_red_bright bg_red_bright)  $(bg_green_bright bg_green_bright)  $(bg_yellow_bright bg_yellow_bright)  $(bg_blue_bright bg_blue_bright)  $(bg_magenta_bright bg_magenta_bright)  $(bg_cyan_bright bg_cyan_bright)  $(bg_white_bright bg_white_bright)"
+  echo
+  echo "注意：当背景色和文字颜色共同使用时，在某些终端下，文字样式会一直保持黑色"
+  echo
+}
+
+# 获取年月日时分秒格式的时间
+get_date() {
+  date '+%Y年%m月%d日 %H时%M分%S秒'
+}
+
 log() {
-  printf "${cyan}[INFO] $(date "+%Y-%m-%d %H:%M:%S")${plain} $1\n"
+  t=$(date "+%Y-%m-%d %H:%M:%S")
+  ft=$(cyan "[INFO] $t")
+  printf "$ft $(white "$1")\n"
 }
 info() {
   printf "$1\n"
-}
-yellow() {
-  printf "${yellow}$1${plain}\n"
-}
-green() {
-  printf "${green}$1${plain}\n"
-}
-red() {
-  printf "${red}$1${plain}\n"
-}
-cyan() {
-  printf "${cyan}$1${plain}\n"
 }
 
 command_exists() {
@@ -103,12 +161,18 @@ command_exists() {
 command_valid() {
   if ! command_exists "$1"; then
     if [ -z "$2" ]; then
-      red "Error: $1 is not installed"
+      red "Error: $1 is not installed or not in PATH"
     else
       red "$2"
     fi
     exit 1
   fi
+}
+
+commands_valid() {
+  for cmd in "$@"; do
+    command_valid "$cmd"
+  done
 }
 
 run() {
@@ -138,15 +202,11 @@ set_var() {
   fi
 }
 
-# 获取年月日时分秒格式的时间
-get_date() {
-  date '+%Y年%m月%d日 %H时%M分%S秒'
-}
-
 # 发送 Webhook 消息
 send_webhook() {
   # 如果不存在 MY_WEBHOOK_URL 环境变量，则不发送消息
   if [ -z "$MY_WEBHOOK_URL" ]; then
+    yellow "MY_WEBHOOK_URL 环境变量不存在"
     return
   fi
 
@@ -158,6 +218,48 @@ send_webhook() {
   local content="$1"
   body="{\"content\":\"$content\"}"
   run "curl -X POST -H 'Content-Type: application/json' -d '$body' \"$MY_WEBHOOK_URL\""
+}
+
+read_confirm() {
+  echo
+  read -p "$(green "$1")" confrim
+  # 如果 $2 为 true，则取消 echo，否则打印
+  [ "$2" = false ] || echo
+
+  case $confrim in
+  [yY] | [yY][eE][sS])
+    return 0
+    ;;
+  [nN] | [nN][oO])
+    return 1
+    ;;
+  *)
+    return 0
+    ;;
+  esac
+}
+
+read_input() {
+  read -p "$(green "$1")" input
+  case $input in
+  "")
+    input="$2"
+    ;;
+  esac
+  echo $input
+}
+
+read_confirm_and_input() {
+  read -p "$(green "$1")" confrim
+  case $confrim in
+  "" | [yY] | [yY][eE][sS])
+    confrim="$2"
+    ;;
+  [nN] | [nN][oO])
+    confrim=""
+    ;;
+  esac
+  echo $confrim
 }
 
 print_arg_warn
